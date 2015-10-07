20a576a62a050f3e4bff541e8b1032428f4ddf08
caorpr <caorpr@gmail.com>
2015-10-07 13:49:57 -0600

    ListViewer, made method that has .get in it.

--- src/list/controller/ArrayListController.java
+++ src/list/controller/ArrayListController.java
@@ -1,10 +1,12 @@
 package list.controller;
 
 import java.util.ArrayList;
+import lists.view.ListsViewer;
 
 public class ArrayListController 
 {
 	private ArrayList<String> firstWords;
+	private ListsViewer popupDisplay;
 	
 	public ArrayListController()
 	{
@@ -14,5 +16,14 @@ public class ArrayListController
 	public void start()
 	{
 		firstWords.add("adfasfsadfjsdlkfjdl;kfjsdak;ls");
+		displayList();
+	}
+	
+	private void displayList()
+	{
+		for(int spot = 0; spot < firstWords.size(); spot++)
+		{
+			popupDisplay.showList("The contents at " + spot + "are " + firstWords.get(spot));
+		}
 	}
 }
