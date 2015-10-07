2f3613a1b8daf88482fea961fcb57920ca98fdfa
caorpr <caorpr@gmail.com>
2015-10-07 13:31:08 -0600

    finished viewer class
    made it work!

--- src/list/controller/ArrayListController.java
+++ src/list/controller/ArrayListController.java
@@ -1,9 +1,18 @@
 package list.controller;
 
+import java.util.ArrayList;
+
 public class ArrayListController 
 {
+	private ArrayList<String> firstWords;
+	
+	public ArrayListController()
+	{
+		firstWords = new ArrayList<String>();
+	}
+	
 	public void start()
 	{
-		
+		firstWords.add("adfasfsadfjsdlkfjdl;kfjsdak;ls");
 	}
 }
---
+++ src/lists/view/ListsViewer.java
@@ -0,0 +1,11 @@
+package lists.view;
+
+import javax.swing.JOptionPane;
+
+public class ListsViewer 
+{
+	public void showList(String currentWords)
+	{
+		JOptionPane.showMessageDialog(null, currentWords);
+	}
+}
